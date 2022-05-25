6c55b1c62b9dc1d231541a1fdb2ac06beae1eb2c
zushe brook <zushe_brook@h-MacBook-Pro-sl-zushe.local>
2022-05-25 16:46:24 +0300

    fixed api

--- backend/app.py
+++ backend/app.py
@@ -12,16 +12,19 @@ app = flask.Flask(__name__)
 def add():
     data = json.loads(flask.request.get_data(as_text=True))
     services.add(user_id=data['user_id'])
+    return "{}", 200
 
 
 @app.route('/users/is_valid/<user_id>', methods=['GET'])
 def is_valid(user_id: str):
-    services.is_valid(user_id=user_id)
+    _is_valid = services.is_valid(user_id=user_id)
+    return json.dumps({"is_valid": _is_valid})
 
 
 @app.route('/users/delete/<user_id>', methods=['DELETE'])
 def delete(user_id: str):
     services.delete(user_id=user_id)
+    return "{}", 200
 
 
 # start the server with the 'run()' method
--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -10,17 +10,30 @@ class TestApp:
     @patch.object(services, 'add')
     def test_get(self, mock_service_get):
         user_id = 'user_id_value'
-        app.post('/users/add', data=json.dumps({"user_id": user_id}))
+        response = app.post('/users/add', data=json.dumps({"user_id": user_id}))
         mock_service_get.assert_called_with(user_id=user_id)
+        assert response.status_code == 200
 
     @patch.object(services, 'delete')
     def test_delete(self, mock_service_delete):
         user_id = 'user_id_value'
-        app.delete(f'/users/delete/{user_id}')
+        response = app.delete(f'/users/delete/{user_id}')
         mock_service_delete.assert_called_with(user_id=user_id)
+        assert response.status_code == 200
 
     @patch.object(services, 'is_valid')
     def test_is_valid(self, mock_service_is_valid):
+        mock_service_is_valid.return_value = True
         user_id = 'user_id_value'
-        app.get(f'/users/is_valid/{user_id}')
+        response = app.get(f'/users/is_valid/{user_id}')
         mock_service_is_valid.assert_called_with(user_id=user_id)
+        assert response.status_code == 200
+        result = json.loads(response.get_data(as_text=True))
+        assert result['is_valid'] is True
+
+        mock_service_is_valid.return_value = False
+        response = app.get(f'/users/is_valid/{user_id}')
+        assert response.status_code == 200
+        result = json.loads(response.get_data(as_text=True))
+        assert result['is_valid'] is False
+
